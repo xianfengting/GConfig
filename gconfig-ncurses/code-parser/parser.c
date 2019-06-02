0c27640041d64fbd8bade028c9ae36fc7eed2e8a
xianfengting <1142631421@qq.com>
2019-06-02 21:02:52 +0800

    Commit some other changes

--- gconfig-ncurses/code-parser/parser.c
+++ gconfig-ncurses/code-parser/parser.c
@@ -3,5 +3,17 @@
 
 #include "parser.h"
 
-void parse_gconfig_code(const char *code)
+static const char *gconfig_head_keywords[] =
+{
+    "mainmenu",
+    "config"
+};
+
+static void parse_mainmenu(const char *mainmenu_code)
 {}
+
+void parse_gconfig_code(const char *code)
+{
+    if (!code)
+        return;
+}
