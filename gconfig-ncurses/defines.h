044aacd1fb82725d2729a8d9922279ad2eace6c2
xianfengting <1142631421@qq.com>
2019-06-07 17:31:45 +0800

    Add defines.h

---
+++ gconfig-ncurses/defines.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This project is licensed under the Anti-996 License.
+
+#ifndef __HEADER_FILE_PROTECTION__DEFINES
+#define __HEADER_FILE_PROTECTION__DEFINES
+
+#define NULL_POINTER_RETURN(name)\
+    if (!name) return;
+
+#endif
