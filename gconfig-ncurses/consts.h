71d309db53f9328903ccb4f2051e9f0b76251e3e
xianfengting <1142631421@qq.com>
2019-06-07 22:46:23 +0800

    Modify entry.c and consts.h to add the screen detection with some other changes.

--- gconfig-ncurses/consts.h
+++ gconfig-ncurses/consts.h
@@ -7,7 +7,12 @@
 enum exit_code
 {
     EXIT_CODE__NORMAL = 0,
-    EXIT_CODE__OUT_OF_MEMORY = 1
+    EXIT_CODE__OUT_OF_MEMORY = 1,
+    EXIT_CODE__ILLEGAL_PROGRAM_ARGS = 2,
+    EXIT_CODE__ILLEGAL_LINES_OR_COLS = 3
 };
+
+#define LEAST_LINES_COUNT ((int) 15)
+#define LEAST_COLS_COUNT ((int) 60)
 
 #endif
--- gconfig-ncurses/entry.c
+++ gconfig-ncurses/entry.c
@@ -3,20 +3,42 @@
 
 #include <ncurses.h>
 #include <signal.h>
+#include <ncurses.h>
 
 #include "main.h"
 #include "screen.h"
+#include "consts.h"
 
 static void signal_handler(int signal)
 {}
+
+static void get_screen_lines_and_cols(int *linesResult, int *colsResult)
+{
+    initscr();
+    *linesResult = LINES;
+    *colsResult = COLS;
+    endwin();
+}
 
 int main(int argc, const char *argv[])
 {
     if (argc < 2)
     {
         fprintf(stderr, "The number of arguments must be at least 2 to run this program.\n");
-        return -1;
+        return EXIT_CODE__ILLEGAL_PROGRAM_ARGS;
     }
+    int lines = 0, cols = 0;
+    printf("GConfig is detecting your screen; please wait and do not do any work on it.\n");
+    get_screen_lines_and_cols(&lines, &cols);
+    printf("GConfig finished detecting your screen.\n");
+    if (lines < LEAST_LINES_COUNT || cols < LEAST_COLS_COUNT)
+    {
+        fprintf(stderr, "The screen is too small to run GConfig; please run GConfig in another larger screen.\n"
+                        "(LINES:%d, at least:%d; COLS:%d, at least:%d)\n",
+                LINES, LEAST_LINES_COUNT, COLS, LEAST_COLS_COUNT);
+        return EXIT_CODE__ILLEGAL_LINES_OR_COLS;
+    }
+
     signal(SIGINT, signal_handler);
     gconfig_parse(argv[1]);
     // screen_init();
