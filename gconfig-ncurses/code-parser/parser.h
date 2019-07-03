ea6988568dd8ff9a7421f915c52fc111ad0e6605
xianfengting <1142631421@qq.com>
2019-07-03 12:57:22 +0800

    Delete the files made of the C language (plan to rewrite them in the C++ language) Delete the C code and rewrite in C++. I thought that the C language was difficult to use, and it's easier for me to write code in the C++ language. So I planned to delete the C code and use C++ to rewrite. Because I found that most of programs nowadays are all made of C++, few programs use C. I used C because I saw Linux and Git are made of C, but now I've already changed my opinion. The C files is in my computer and I didn't delete them.

--- gconfig-ncurses/code-parser/parser.h
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// This project is licensed under the Anti-996 License.
-
-#ifndef __HEADER_FILE_PROTECTION__PARSER
-#define __HEADER_FILE_PROTECTION__PARSER
-
-extern void parse_gconfig_code(const char *code, const char *fileFolder);
-
-#endif
