83242bf7d3ce7c46e871ec2cf1df9d724e2bbe0b
xianfengting <1142631421@qq.com>
2019-07-03 15:32:56 +0800

    Modify code-parser

--- gconfig-ncurses/code-parser/code-parser.cpp
+++ gconfig-ncurses/code-parser/code-parser.cpp
@@ -1,9 +1,50 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "../exception.h"
 #include "code-parser.h"
+
+using namespace std;
 
 CodeParser::CodeParser()
 {}
 
 CodeParser::~CodeParser()
+{}
+
+void CodeParser::AddFile(const_str_t filePath)
+{
+    AddFile(string(filePath));
+}
+
+void CodeParser::AddFile(const string &filePath)
+{
+    ifstream file;
+    file.open(filePath);
+    if (!file.is_open()) {
+        string msg;
+        msg += "Can't open the file \"";
+        msg += filePath;
+        msg += "\".";
+        throw Exception(msg);
+    }
+    file >> noskipws; // Read the file including space and newline characters.
+    string content;
+    while (!file.eof()) {
+        char c;
+        file >> c;
+        char str[2];
+        sprintf(str, "%c", c);
+        content.append(str);
+    }
+    AddStringContent(content);
+}
+
+void CodeParser::AddStringContent(const_str_t strContent)
+{}
+
+void CodeParser::AddStringContent(const string &strContent)
 {}
--- gconfig-ncurses/code-parser/code-parser.h
+++ gconfig-ncurses/code-parser/code-parser.h
@@ -3,13 +3,21 @@
 #ifndef __HEADER_FILE_PROTECTION__CODE_PARSER_H
 #define __HEADER_FILE_PROTECTION__CODE_PARSER_H
 
+#include <string>
+
 #include "../object.h"
+#include "../typedefs.h"
 
 class CodeParser : public Object
 {
 public:
     CodeParser();
     virtual ~CodeParser();
+
+    void AddFile(const_str_t filePath);
+    void AddFile(const std::string &filePath);
+    void AddStringContent(const_str_t strContent);
+    void AddStringContent(const std::string &strContent);
 };
 
 #endif
